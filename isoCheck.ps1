0f7e9513361834de8355a1d7e6914fca18232013
Filadeus <danilefimowscky@gmail.com>
2022-06-18 22:53:36 +0300

    Updated isoCheck.ps1 and Dockerfile

--- isoCheck.ps1
+++ isoCheck.ps1
@@ -2,6 +2,6 @@ if (Test-Path -Path .\windows10.iso -PathType Leaf){
     Write-Output "ISO present. Skipping!"
 }
 else {
-    Invoke-Item (start powershell(.\Fido.ps1 -Win 10 -Ed Pro -Lang English International))
+    Invoke-Item (.\Fido.ps1 -Win 10 -Ed Pro -Lang English International)
     Rename-Item -Path ".\Win*.iso" -NewName "windows10.iso"
 }
