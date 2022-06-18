6e3556e9dcf53ea7b3cdabf9d8f2c60fc4eaecf1
Filadeus <danilefimowscky@gmail.com>
2022-06-19 00:43:05 +0300

    Updated isoCheck.ps1 and Dockerfile

--- isoCheck.ps1
+++ isoCheck.ps1
@@ -1,8 +1,10 @@
 if (Test-Path -Path .\windows10.iso -PathType Leaf){
-    Write-Output "ISO present. Skipping!"
+    Write-Output "[$(Get-Date)] ISO present. Skipping!"
 }
 else {
-    Write-Output "ISO wasn't found. Downloading..."
+    Write-Output "[$(Get-Date)] ISO wasn't found. Downloading..."
     & .\Fido.ps1 -Win 10 -Ed Pro -Lang English International
+    Write-Output "[$(Get-Date)] ISO downloaded."
     Rename-Item -Path ".\Win*.iso" -NewName "windows10.iso"
+    Write-Output "[$(Get-Date)] Done."
 }
