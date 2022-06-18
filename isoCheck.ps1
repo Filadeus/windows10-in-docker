94f6f67d62cc7f71f65fa4f3dd353fb12997fa63
Filadeus <danilefimowscky@gmail.com>
2022-06-18 22:24:33 +0300

    Updated isoCheck.ps1 and Dockerfile

--- isoCheck.ps1
+++ isoCheck.ps1
@@ -3,5 +3,5 @@ if (Test-Path -Path .\windows10.iso -PathType Leaf){
 }
 else {
     Invoke-Item (start powershell(.\Fido.ps1 -Win 10 -Ed Pro -Lang English International))
-    bash find . -type f -name "Win10*.iso" -exec \'sh -c x="{}"; mv "$x" "windows10.iso" \' \\;
+    Rename-Item -Path ".\Win*.iso" -NewName "windows10.iso"
 }
